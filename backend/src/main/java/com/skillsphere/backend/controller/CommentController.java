05a211a02953025929b0ba6f13de899e748b2866
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-06 12:19:14 +0530

    added comment controller

--- backend/src/main/java/com/skillsphere/backend/controller/CommentController.java
+++ backend/src/main/java/com/skillsphere/backend/controller/CommentController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
+//ctrl
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
