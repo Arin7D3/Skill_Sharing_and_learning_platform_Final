b83bdecf6603612da1716fef073c3e3d07e86d41
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-06 12:23:22 +0530

    added user controller

--- backend/src/main/java/com/skillsphere/backend/controller/UserController.java
+++ backend/src/main/java/com/skillsphere/backend/controller/UserController.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//user
 import java.util.HashMap;
 import java.util.Map;
 
