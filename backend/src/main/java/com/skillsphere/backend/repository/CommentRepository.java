47b0f4273f9fc154883429af08da62059edb15db
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-06 12:21:31 +0530

    changed repo in comments

--- backend/src/main/java/com/skillsphere/backend/repository/CommentRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/CommentRepository.java
@@ -2,7 +2,7 @@ package com.skillsphere.backend.repository;
 
 import com.skillsphere.backend.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-//repo
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     void deleteByPostId(Long postId);
 }
