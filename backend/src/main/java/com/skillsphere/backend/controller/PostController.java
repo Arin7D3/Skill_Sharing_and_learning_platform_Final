88d004e048eb29a17a83efcdb4b7bffa752698c3
TharushiDhananga <mbtharushidhananga@gmail.com>
2025-05-06 22:40:16 +0530

    Add error handling

--- backend/src/main/java/com/skillsphere/backend/controller/PostController.java
+++ backend/src/main/java/com/skillsphere/backend/controller/PostController.java
@@ -82,7 +82,7 @@ public class PostController {
             return ResponseEntity.notFound().build();
         }
     }
-    //update post  sdgg
+    //update post  
     @PutMapping("/{id}")
     public ResponseEntity<?> updatePost(
             @PathVariable Long id,
