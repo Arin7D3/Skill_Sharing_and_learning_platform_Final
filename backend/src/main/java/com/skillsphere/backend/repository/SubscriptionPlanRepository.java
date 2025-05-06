e3df5b24bebfe0050ed828eb9ae5fbfb69eb702f
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-06 19:33:37 +0530

    added new plan

--- backend/src/main/java/com/skillsphere/backend/repository/SubscriptionPlanRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/SubscriptionPlanRepository.java
@@ -3,7 +3,7 @@ package com.skillsphere.backend.repository;
 
 import com.skillsphere.backend.model.SubscriptionPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 import java.util.Optional;
 
 public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
