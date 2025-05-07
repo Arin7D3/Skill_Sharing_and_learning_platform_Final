52d9b788025d04f5e77f662b0b017a157bdd87e4
bishna shohani <bishnashoha11@gmail.com>
2025-05-07 14:13:24 +0530

    update Repository

--- backend/src/main/java/com/skillsphere/backend/repository/LearningPlanRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/LearningPlanRepository.java
@@ -4,7 +4,7 @@ import com.skillsphere.backend.model.LearningPlan;
 import com.skillsphere.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//
 import java.util.List;
 
 @Repository
