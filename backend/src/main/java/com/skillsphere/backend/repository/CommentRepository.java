6ab8e521f9d263d87c348060899e41d7d43cc23f
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-16 00:08:11 +0530

    done

--- backend/src/main/java/com/skillsphere/backend/repository/CommentRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/CommentRepository.java
@@ -2,7 +2,7 @@ package com.skillsphere.backend.repository;
 
 import com.skillsphere.backend.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-//
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     void deleteByPostId(Long postId);
 }
