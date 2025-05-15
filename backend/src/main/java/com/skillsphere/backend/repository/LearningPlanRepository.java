2bb61f9203135497b3e5b321f86810aa610fa32f
bishna shohani <bishnashoha11@gmail.com>
2025-05-15 09:28:40 +0530

    change a name

--- backend/src/main/java/com/skillsphere/backend/repository/LearningPlanRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/LearningPlanRepository.java
@@ -1,10 +1,8 @@
 package com.skillsphere.backend.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.skillsphere.backend.model.LearningPlan;
 import com.skillsphere.backend.model.User;
 
