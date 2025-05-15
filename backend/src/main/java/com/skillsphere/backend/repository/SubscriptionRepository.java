2c69db8f67ef293c2d29f444ce1135dd454cac0f
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-05-16 00:06:46 +0530

    change newly added things

--- backend/src/main/java/com/skillsphere/backend/repository/SubscriptionRepository.java
+++ backend/src/main/java/com/skillsphere/backend/repository/SubscriptionRepository.java
@@ -3,7 +3,7 @@ package com.skillsphere.backend.repository;
 
 import com.skillsphere.backend.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 import java.util.List;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
