1012156f5c7162ae1098c25495858d99b48f8cc7
TharushiDhananga <mbtharushidhananga@gmail.com>
2025-05-16 00:22:21 +0530

    update dto

--- backend/src/main/java/com/skillsphere/backend/controller/UserController.java
+++ backend/src/main/java/com/skillsphere/backend/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+//fewfuwehw
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
