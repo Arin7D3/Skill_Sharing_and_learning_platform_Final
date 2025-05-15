670565014d4b2755329e65095d6173c721e19d5e
bishna shohani <bishnashoha11@gmail.com>
2025-05-15 09:29:52 +0530

    set nullabel

--- backend/src/main/java/com/skillsphere/backend/model/LearningPlan.java
+++ backend/src/main/java/com/skillsphere/backend/model/LearningPlan.java
@@ -13,7 +13,7 @@ public class LearningPlan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//
+
     @Column(nullable = false)
     private String title;
 
