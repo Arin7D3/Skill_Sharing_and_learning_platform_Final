0f188847af6c3e4dd27b3619cde4f2a74c6b47e9
bishna shohani <bishnashoha11@gmail.com>
2025-05-15 15:41:57 +0530

    update a comment

--- backend/src/main/java/com/skillsphere/backend/dto/LearningPlanDTO.java
+++ backend/src/main/java/com/skillsphere/backend/dto/LearningPlanDTO.java
@@ -1,18 +1,17 @@
+//transfer data between the backend and frontend 
 package com.skillsphere.backend.dto;
-//
 import lombok.Data;
-
 import java.time.LocalDateTime;
 
-
+//explain attibutes and their purpose
 @Data
 public class LearningPlanDTO {
     private Long id;
     private String title;
     private String description;
     private Integer duration;
-    private String thumbnailUrl; // New field
-    private String status; // New field
+    private String thumbnailUrl; 
+    private String status; 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private Long userId;
